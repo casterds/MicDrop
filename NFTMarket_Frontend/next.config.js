40457f8e90c494e770e51ef5bd5f28e3ae3deccf
casterds <96631866+casterds@users.noreply.github.com>
2023-05-06 01:06:20 +0530

    Update next.config.js

--- NFTMarket_Frontend/next.config.js
+++ NFTMarket_Frontend/next.config.js
@@ -1,2 +1,16 @@
 const withImages = require('next-images');
-module.exports = withImages()
+
+module.exports = {
+  images: withImages(),
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        "fs": false,
+        "net": false,
+        "tls": false
+      }
+    }
+    return config
+  }
+};
